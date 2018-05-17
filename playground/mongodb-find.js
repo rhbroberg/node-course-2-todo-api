ea791a7966d5b5e0ee328fbe86b0f992fd24fee0
Richard Broberg <rhb@brobasino.com>
2018-05-17 15:07:47 -0400

    initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect to mongodb server', err);
+    }
+    console.log('connected to mongodb server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5afb4b414c4efb7328377cc4')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('unable to fetch todos', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('unable to fetch todos', err);
+    // });
+
+    db.collection('Users').find({ name: 'rhb' }).toArray().then((docs) => {
+        console.log('my named docs are');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('bummer - error', err);
+    });
+
+    // db.close();
+});
---
+++ playground/monogdb-connect.js
@@ -0,0 +1,37 @@
+//const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('unable to connect to mongodb server', err);
+    }
+    console.log('connected to mongodb server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'something to do',
+    //     completed: false
+
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('unable to insert todo', err);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    // insert new doc into Users (name, age, location-string)
+
+    // db.collection('Users').insertOne({
+    //     name: 'rhb',
+    //     age: 49,
+    //     location: '02482'
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log('unable to insert into Users', err);
+    //     }
+
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
+
+    db.close();
+});
