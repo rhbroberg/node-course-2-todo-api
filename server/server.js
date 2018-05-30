e5dcfb06abfd7afcff59ffc22eb6ce9b0c8d7839
Richard Broberg <rhb@brobasino.com>
2018-05-30 18:44:32 -0400

    added x-auth authentication for GET /users/me

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var { mongoose } = require('./db/mongoose'); // eslint-disable-line no-unused-vars
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+var { authenticate } = require('./middleware/authenticate');
+
 const { ObjectID } = require('mongodb');
 
 var app = express();
@@ -104,6 +106,11 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
