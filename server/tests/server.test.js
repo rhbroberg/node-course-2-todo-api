6a6574d720d614a35b9261796bf90ecdcb04d282
Richard Broberg <rhb@brobasino.com>
2018-06-11 18:56:06 -0400

    added tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -193,7 +193,7 @@ describe('GET /users/me', () => {
             .end(done);
     });
 
-    it('should return 401 if not authentcated', (done) => {
+    it('should return 401 if not authenticated', (done) => {
         // body should be empty, .toEqual
         request(app)
             .get('/users/me')
@@ -302,6 +302,25 @@ describe('POST /users/login', () => {
                 }
 
                 User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => { // eslint-disable-line no-unused-vars
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
                     expect(user.tokens.length).toBe(0);
                     done();
                 }).catch((e) => done(e));
